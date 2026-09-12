a1878eb5acce5a3760f347e155ff841748bca2c7
stevenchacon733-arch <steven.chacon.733@gmail.com>
2026-09-11 21:28:21 -0600

    Add files via upload

---
+++ app/admin/page.js
@@ -0,0 +1,10 @@
+import AdminPanel from "@/components/AdminPanel";
+
+export const metadata = {
+  title: "PrecioCR Admin",
+  robots: "noindex,nofollow",
+};
+
+export default function AdminPage() {
+  return <AdminPanel />;
+}
